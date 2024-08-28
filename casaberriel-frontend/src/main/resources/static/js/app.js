73215bb960413b0be21938e53eb71f9131fc486d
ignferna78 <ignacio.fernandez-villar@capgemini.com>
2024-08-28 11:55:35 +0200

    ajustes estructura proyecto parte1

---
+++ casaberriel-frontend/src/main/resources/static/js/app.js
@@ -0,0 +1,11 @@
+//*Metodo para confirmar eliminar una reserva
+$(document).ready(function() {
+	$('#deleteModal').on('show.bs.modal', function (event) {
+	  let button = $(event.relatedTarget); // Botón que activó el modal
+	  let id = button.data('id'); // Extrae la información de data-id
+
+	  let modal = $(this);
+	  let deleteUrl = '/reservas/eliminar/' + id;
+	  modal.find('#confirmDeleteButton').attr('href', deleteUrl); // Asigna la URL de eliminación al botón
+	});
+});
