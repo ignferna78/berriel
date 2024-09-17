3672bd5bfdbdcbc52b70b9212a05ae914e5e6922
ignferna78 <ignacio.fernandez-villar@capgemini.com>
2024-09-17 10:28:23 +0200

    ajuste mapa ubicación // Implementación boton subir

--- casaberriel-frontend/src/main/resources/static/js/app.js
+++ casaberriel-frontend/src/main/resources/static/js/app.js
@@ -8,25 +8,6 @@ $('#deleteModal').on('show.bs.modal', function (event) {
 	  let deleteUrl = '/reservas/eliminar/' + id;
 	  modal.find('#confirmDeleteButton').attr('href', deleteUrl); // Asigna la URL de eliminación al botón
 	});
-	
-
-function initMap() {
-	// Coordenadas de tu localización (latitud y longitud)
-	const location = {lat: 28.956167, lng: -13.640944}; // Reemplaza con las coordenadas reales de tu ubicación
-
-	// Crear el mapa centrado en la localización
-	const map = new google.maps.Map(document.getElementById("map"), {
-		zoom: 15,
-		center: location,
-	});
-
-	// Añadir un marcador en la localización
-	const marker = new google.maps.Marker({
-		position: location,
-		map: map,
-	});
-}
-
 
 	
 	function limpiarCriterios() {
@@ -41,3 +22,4 @@ function initMap() {
 	         });
 	     }
 	
+
